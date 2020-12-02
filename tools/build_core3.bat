bebf350d9911b1b522be55694be11fb784084997
k-matsuzawa <tangrambs803@gmail.com>
2020-12-02 14:36:03 +0900

    feat: update script

--- tools/build_core3.bat
+++ tools/build_core3.bat
@@ -4,10 +4,7 @@ if exist "build_core3.bat" (
   cd ..
 )
 
-REM set PATH=%PATH:C:\Program Files\Git\usr\bin;=%
-REM set PATH=%PATH:C:\Program Files (x86)\Git\usr\bin;=%
-
-CALL cmake -S . -B build -A x64 -DENABLE_CSHARP=off -DENABLE_SHARED=on -DENABLE_JS_WRAPPER=off -DENABLE_CAPI=on -DENABLE_TESTS=off -DCMAKE_BUILD_TYPE=Release
+CALL cmake -S . -B build -A x64 -DENABLE_SHARED=on -DENABLE_JS_WRAPPER=off -DENABLE_CAPI=on -DENABLE_TESTS=off -DCMAKE_BUILD_TYPE=Release
 if not %ERRORLEVEL% == 0 (
     exit 1
 )
